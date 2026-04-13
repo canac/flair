dd5ef4a16330197f9ba0380e41dab567a5460ea2
Caleb Cox <canac@users.noreply.github.com>
2026-04-13 10:54:55 -0500

    Use $PORT

--- vite.config.ts
+++ vite.config.ts
@@ -3,4 +3,7 @@ import { fresh } from "@fresh/plugin-vite";
 
 export default defineConfig({
   plugins: [fresh()],
+  server: {
+    port: process.env.PORT ? Number(process.env.PORT) : undefined,
+  },
 });
