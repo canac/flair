a597ea1db10e3f6855c6dd2619ba398b76773417
Caleb Cox <canac@users.noreply.github.com>
2026-06-07 13:57:11 -0500

    Fix island CSS not being loaded

--- client.ts
+++ client.ts
@@ -1,6 +1,3 @@
 // Import CSS files here for hot module reloading to work.
 import "./assets/styles.css";
 import "./routes/_app.css";
-import "./routes/index.css";
-import "./routes/recipes/[id].css";
-import "./islands/AdjustmentsEditor/AdjustmentsEditor.css";
